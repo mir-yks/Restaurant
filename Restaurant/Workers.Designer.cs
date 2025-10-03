6049f5d6a7a3e349c4440749dd64ce87171bc595
Mudrov Maksim <mudrov010203@gmail.com>
2025-10-03 08:40:34 +0300

    Изменил положение кнопок на форме сотрудников

--- Restaurant/Workers.Designer.cs
+++ Restaurant/Workers.Designer.cs
@@ -92,7 +92,7 @@
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Verdana", 14F);
-            this.button3.Location = new System.Drawing.Point(513, 494);
+            this.button3.Location = new System.Drawing.Point(543, 497);
             this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(110, 50);
@@ -107,7 +107,7 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Verdana", 14F);
-            this.button2.Location = new System.Drawing.Point(646, 494);
+            this.button2.Location = new System.Drawing.Point(661, 497);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(110, 50);
@@ -122,7 +122,7 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Verdana", 14F);
-            this.button1.Location = new System.Drawing.Point(13, 494);
+            this.button1.Location = new System.Drawing.Point(13, 497);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 50);
